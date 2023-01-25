1bd43537fc04e5acff9f52aca756a27b9a76013b
Jason <jason.johnson.081@gmail.com>
2023-01-25 14:34:57 +0000

    update python app

--- demo/message-sender/app.py
+++ demo/message-sender/app.py
@@ -14,3 +14,48 @@ async def send_single_message(sender):
     # send the message to the topic
     await sender.send_messages(message)
     print("Sent a single message")
+
+async def send_a_list_of_messages(sender):
+    # Create a list of messages
+    messages = [ServiceBusMessage("Message in list") for _ in range(5)]
+    # send the list of messages to the topic
+    await sender.send_messages(messages)
+    print("Sent a list of 5 messages")
+
+async def send_batch_message(sender):
+    # Create a batch of messages
+    async with sender:
+        batch_message = await sender.create_message_batch()
+        for _ in range(10):
+            try:
+                # Add a message to the batch
+                batch_message.add_message(ServiceBusMessage("Message inside a ServiceBusMessageBatch"))
+            except ValueError:
+                # ServiceBusMessageBatch object reaches max_size.
+                # New ServiceBusMessageBatch object can be created here to send more data.
+                break
+        # Send the batch of messages to the topic
+        await sender.send_messages(batch_message)
+    print("Sent a batch of 10 messages")
+
+async def run():
+    # create a Service Bus client using the credential.
+    async with ServiceBusClient(
+        fully_qualified_namespace=FULLY_QUALIFIED_NAMESPACE,
+        credential=credential,
+        logging_enable=True) as servicebus_client:
+        # Get a Topic Sender object to send messages to the topic
+        sender = servicebus_client.get_topic_sender(topic_name=TOPIC_NAME)
+        async with sender:
+            # Send one message
+            await send_single_message(sender)
+            # Send a list of messages
+            await send_a_list_of_messages(sender)
+            # Send a batch of messages
+            await send_batch_message(sender)
+        # Close credential when no longer needed.
+        await credential.close()
+
+asyncio.run(run())
+print("Done sending messages")
+print("-----------------------")
