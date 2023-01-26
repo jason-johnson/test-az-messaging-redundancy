1344e1f5e160832c17da6b8c5f361c9470554271
Jason <jason.johnson.081@gmail.com>
2023-01-26 17:32:54 +0000

    forgot versions file for module

---
+++ terraform/function/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.34"
+    }
+    namep = {
+      source  = "jason-johnson/namep"
+      version = "~> 1.0"
+    }
+  }
+}
