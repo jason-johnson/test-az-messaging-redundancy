12fb50297c707f9611fded275a14f2045a0286c2
Jason <jason.johnson.081@gmail.com>
2023-01-27 11:11:17 +0000

    turn off function 2 for now

--- terraform/function.tf
+++ terraform/function.tf
@@ -12,15 +12,15 @@ module "main-function" {
     servicebus_topic_id = azurerm_servicebus_topic.main.id
 }
 
-module "second-function" {
-    source = "./function"
-    name = "second"
-    resource_group_name = azurerm_resource_group.rg.name
-    resource_group_location = azurerm_resource_group.rg.location
-    app_insights_connection_string = azurerm_application_insights.main.connection_string
-    app_insights_instrumentation_key = azurerm_application_insights.main.instrumentation_key
-    acr_login_server = azurerm_container_registry.acr.login_server
-    acr_id = azurerm_container_registry.acr.id
-    servicebus_namespace_name = azurerm_servicebus_namespace.main.name
-    servicebus_topic_id = azurerm_servicebus_topic.main.id
-}
+#module "second-function" {
+#    source = "./function"
+#    name = "second"
+#    resource_group_name = azurerm_resource_group.rg.name
+#    resource_group_location = azurerm_resource_group.rg.location
+#    app_insights_connection_string = azurerm_application_insights.main.connection_string
+#    app_insights_instrumentation_key = azurerm_application_insights.main.instrumentation_key
+#    acr_login_server = azurerm_container_registry.acr.login_server
+#    acr_id = azurerm_container_registry.acr.id
+#    servicebus_namespace_name = azurerm_servicebus_namespace.main.name
+#    servicebus_topic_id = azurerm_servicebus_topic.main.id
+#}
