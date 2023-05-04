79d381baf05f4c4f354a87e6c6dc577c5e3c994d
Jason <jason.johnson.081@gmail.com>
2023-05-04 09:33:46 +0000

    got rid of things we don't need right now

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,22 +2,6 @@ output "rg_name" {
   description = "Name of project resource group"
   value       = azurerm_resource_group.rg.name
 }
-
-output "main_func_sa_name" {
-  description = "Name of project storage account"
-  value       = module.main-function.sa_name
-}
-
-output "main_func_sp_name" {
-  description = "Name of project service plan"
-  value       = module.main-function.sp_name
-}
-
-output "main_fun_name" {
-  description = "Name of project azure function app"
-  value       = module.main-function.function_name
-}
-
 output "acr_name" {
   description = "Name of project azure container registry"
   value       = azurerm_container_registry.acr.name
